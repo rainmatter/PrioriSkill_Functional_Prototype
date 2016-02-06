b009efd71733330c3ff857f22738eb78a3d962b8
Kim S <stewak5@uw.edu>
2016-02-06 15:53:46 -0800

    Added buttons to view job on side. Moved header to index.

--- app/scripts/controllers/jobs.js
+++ app/scripts/controllers/jobs.js
@@ -10,12 +10,20 @@
 angular.module('prioriSkillPrototypeApp')
   .controller('JobsController', ['$scope', 'jobsFactory', function ($scope, jobsFactory) {
 	  	$scope.showDetails = false;
+		$scope.showJob = false;
+		$scope.jobViewed = {id:'', date:'', title:'', company:'', description:'', shortDescription:'', words:'' };
 		
 		$scope.toggleJobDetails = function() {
             $scope.showDetails = !$scope.showDetails;
         };
 		
 		$scope.jobs = jobsFactory.getJobs();
+		$scope.toggleViewJob = function(jobID) {
+			console.log(jobID);
+			$scope.jobViewed = jobsFactory.getJob(jobID);
+			$scope.showJob = true;
+		};
+		
 		
 		var jobsOrder = 'date';
 		$scope.jobsOrder = jobsOrder;
@@ -27,15 +35,16 @@ angular.module('prioriSkillPrototypeApp')
   
   
   .controller('JobFormController', ['$scope', function($scope) {
-		$scope.newJob = {date:'', title:'', company:'', description:'', shortDescription:'', words:'' };
+		$scope.newJob = {id:'', date:'', title:'', company:'', description:'', shortDescription:'', words:'' };
 							 
 		$scope.submitNewJob = function () {
 			console.log('clickie!');
 			$scope.newJob.date = new Date().toISOString();
+			$scope.newJob.id = $scope.jobs.length;
 			$scope.newJob.shortDescription = shortenDescription($scope.newJob.description);
 			$scope.jobs.push($scope.newJob);
 			$scope.newJobForm.$setPristine();
-			$scope.newJob = {date:'', title:'', company:'', description:'', shortDescription:'', words:'' };
+			$scope.newJob = {id: '', date:'', title:'', company:'', description:'', shortDescription:'', words:'' };
 			console.log($scope.jobs);
 		};
 		
--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -5,14 +5,16 @@ angular.module('prioriSkillPrototypeApp')
 			var jobFac = {};
 		
 			var jobs = [
-			{	date:'2016-01-25T17:57:28.556094Z',
+			{	id: 0,
+				date:'2016-01-25T17:57:28.556094Z',
 				title:'Web Developer',
 				company:'Some Company', 
 				description:'We are looking for someone with expertise in HTML, CSS, JavaScript, and Angular.',
 				shortDescription:'We are looking for...',
 				words: 'HTML, CSS, JavaScript, Angular'
 			}, 
-			{	date:'2016-02-01T17:57:28.556094Z',
+			{	id: 1,
+				date:'2016-02-01T17:57:28.556094Z',
 				title:'Java Developer',
 				company:'Another Company', 
 				description:'Applicant must have experience with OOP, Java, and Git version control.',
@@ -24,6 +26,10 @@ angular.module('prioriSkillPrototypeApp')
 		
 			jobFac.getJobs = function () {
 				return jobs;
+			};
+			
+			jobFac.getJob = function (id) {
+				return jobs[id];
 			};
 
 			return jobFac;
