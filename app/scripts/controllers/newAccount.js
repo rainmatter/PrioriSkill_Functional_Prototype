97d7b6be0765f400efa0820f2afaef171dea4510
Kim S <stewak5@uw.edu>
2016-02-11 01:45:01 -0800

    Working on creating new account. Need method of checking if email exists in database.

--- app/scripts/controllers/newAccount.js
+++ app/scripts/controllers/newAccount.js
@@ -24,24 +24,50 @@ angular.module('prioriSkillPrototypeApp')
 		
 		}])
   .controller('NewAccountController', ['$scope', 'accountFactory', function ($scope, accountFactory) {
+	  
 		var newAccount = {name:'', email:'', password:'', dateCreated:'', dateLastLogin:'', jobs:[], skills:[]};
 		$scope.newAccount = newAccount;
 		$scope.pwordConfirm = '';
+		$scope.emailInUse = true;
+		$scope.showEmailInUse = false;	
 				
-				
+				$scope.anAccount = accountFactory.getAccount().query({email: 'email@email.com'});
+				console.log(accountFactory.getAccount().query({email: 'email@email.com'}).length);
 		
 		$scope.submitNewAccount = function() {
-			$scope.newAccount.dateCreated = new Date().toISOString();
-			$scope.newAccount.dateLastLogin = new Date().toISOString();
-			accountFactory.getAccounts().save($scope.newAccount);
-			$scope.accounts.push($scope.newAccount);
-			$scope.newAccount = {name:'', email:'', password:'', dateCreated:'', dateLastLogin:'', jobs:[], skills:[]};
-			$scope.newAccountForm.$setPristine();
-			$scope.pwordConfirm = '';
-			//console.log($scope.accounts);
+			$scope.testEmailInUse($scope.newAccount.email);
+			if (!$scope.emailInUse) {
+				$scope.newAccount.dateCreated = new Date().toISOString();
+				$scope.newAccount.dateLastLogin = new Date().toISOString();
+				accountFactory.getAccounts().save($scope.newAccount);
+				$scope.accounts.push($scope.newAccount);
+				$scope.newAccount = {name:'', email:'', password:'', dateCreated:'', dateLastLogin:'', jobs:[], skills:[]};
+				$scope.newAccountForm.$setPristine();
+				$scope.pwordConfirm = '';
+				$scope.emailInUse = false;
+				$scope.showEmailInUse = false;
+				//console.log($scope.accounts);
+			} else {
+				$scope.showEmailInUse = true;
+			}
 		};
 		
-		$scope.anAccount = {};
+		$scope.testEmailInUse = function(newEmail) {
+			$scope.anAccount = accountFactory.getAccount().query({email: '' + newEmail + ''});
+				console.log($scope.anAccount.length);
+						
+						if ($scope.anAccount.length > 0){
+							console.log('email already in use!');
+							$scope.emailInUse = true;
+							$scope.showEmailInUse = true;
+						} else {
+							console.log('good to go!');
+							$scope.emailInUse = false;
+							$scope.showEmailInUse = false;
+						}
+		};
+		
+		/*$scope.anAccount = {};
             $scope.anAccount = accountFactory.getAccount().query({email: 'email@email.com'})
                 .$promise.then(
                     function (response) {
@@ -53,7 +79,7 @@ angular.module('prioriSkillPrototypeApp')
                         console.log('Error: ' + response.status + ' ' + response.statusText);
 						console.log('good to go!');
                     }
-                );
+                );*/
 		
 
 	
